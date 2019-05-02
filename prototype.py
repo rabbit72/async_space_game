084b9fea30c2267c35671bbe965472d843aad75d
Daniil Shadrin <rabbit72rus@gmail.com>
2019-05-03 00:26:08 +0300

    add fire

--- prototype.py
+++ prototype.py
@@ -8,6 +8,36 @@ from curses import curs_set
 TIC_TIMEOUT = 0.1
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot. Direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas, row, column, symbol="*"):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -55,13 +85,16 @@ def draw(canvas):
     canvas.border()
     curs_set(False)
     coroutines = [star for star in generate_stars(canvas, 100)]
+    center_row, center_column = curses.LINES // 2, curses.COLS // 2
+    coroutines.append(fire(canvas, center_row, center_column))
     while True:
-        for star in coroutines:
+        for coroutine in coroutines:
             try:
-                star.send(None)
+                coroutine.send(None)
             except StopIteration:
-                coroutines.remove(star)
+                coroutines.remove(coroutine)
 
+        canvas.border()
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
