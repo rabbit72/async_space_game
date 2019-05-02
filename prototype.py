469c2f13da201071601d4de25b0b39260aadfa71
Daniil Shadrin <rabbit72rus@gmail.com>
2019-05-02 23:52:19 +0300

    add independent behavior of stars

--- prototype.py
+++ prototype.py
@@ -11,6 +11,11 @@ TIC_TIMEOUT = 0.1
 async def blink(canvas, row, column, symbol="*"):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
+        start_offset = random.randint(0, 30)
+        for _ in range(start_offset):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_DIM)
         for _ in range(20):
             await asyncio.sleep(0)
 
