fb2191ecc3b19bef71d15d46055ae68cbec8559e
Daniil Shadrin <rabbit72rus@gmail.com>
2019-05-03 00:46:32 +0300

    move star logic to separated module

---
+++ animations/stars.py
@@ -0,0 +1,43 @@
+import random
+import asyncio
+import curses
+
+
+def generate_stars(canvas, quantity: int, symbols: iter = "+*.:") -> list:
+    used_coordinates = set()
+    min_row, min_column = 1, 1
+    max_y, max_x = canvas.getmaxyx()
+    max_row, max_column = max_y - 2, max_x - 2
+
+    for _ in range(quantity):
+
+        row = random.randint(min_row, max_row)
+        column = random.randint(min_row, max_column)
+        while (row, column) in used_coordinates:
+            row = random.randint(min_row, max_row)
+            column = random.randint(min_row, max_column)
+        used_coordinates.add((row, column))
+
+        symbol = random.choice(symbols)
+        yield blink(canvas, row, column, symbol)
+
+
+async def blink(canvas, row, column, symbol="*"):
+    while True:
+
+        start_offset = random.randint(10, 25)
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in range(start_offset):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        for _ in range(5):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
--- prototype.py
+++ prototype.py
@@ -1,31 +1,11 @@
 import time
-import random
 import curses
 from curses import wrapper
 from curses import curs_set
 from animations.fire import fire
-from animations.blink import blink
+from animations.stars import generate_stars
 
 TIC_TIMEOUT = 0.1
-
-
-def generate_stars(canvas, quantity: int, symbols: iter = "+*.:") -> list:
-    used_coordinates = set()
-    min_row, min_column = 1, 1
-    max_y, max_x = canvas.getmaxyx()
-    max_row, max_column = max_y - 2, max_x - 2
-
-    for _ in range(quantity):
-
-        row = random.randint(min_row, max_row)
-        column = random.randint(min_row, max_column)
-        while (row, column) in used_coordinates:
-            row = random.randint(min_row, max_row)
-            column = random.randint(min_row, max_column)
-        used_coordinates.add((row, column))
-
-        symbol = random.choice(symbols)
-        yield blink(canvas, row, column, symbol)
 
 
 def draw(canvas):
