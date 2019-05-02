f0f42c5d82caa4f5d143060d5538aa26c312658a
Daniil Shadrin <rabbit72rus@gmail.com>
2019-05-03 01:42:11 +0300

    decrease spaceship animation time

--- animations/spaceship.py
+++ animations/spaceship.py
@@ -8,7 +8,7 @@ async def animate_spaceship(canvas, start_row: int, start_column: int, frames: l
             draw_frame(canvas, start_row, start_column, frame)
             canvas.refresh()
 
-            await async_sleep(10)
+            await async_sleep(2)
 
             # стираем предыдущий кадр, прежде чем рисовать новый
             draw_frame(canvas, start_row, start_column, frame, negative=True)
