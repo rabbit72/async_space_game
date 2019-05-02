bef6332cc53969574040e8fed46a88c936b0da8a
Daniil Shadrin <rabbit72rus@gmail.com>
2019-05-03 00:40:54 +0300

    change start offset behavior

--- animations/blink.py
+++ animations/blink.py
@@ -5,13 +5,10 @@ import curses
 
 async def blink(canvas, row, column, symbol="*"):
     while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        start_offset = random.randint(0, 20)
-        for _ in range(start_offset):
-            await asyncio.sleep(0)
 
+        start_offset = random.randint(10, 25)
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(20):
+        for _ in range(start_offset):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
