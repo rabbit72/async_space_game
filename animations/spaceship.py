c571c2db20a2a1f0fd39e846d9377dcd44a2a089
Daniil Shadrin <rabbit72rus@gmail.com>
2019-05-05 20:20:38 +0300

    add spaceship moving

--- animations/spaceship.py
+++ animations/spaceship.py
@@ -1,14 +1,22 @@
-from curses_tools import draw_frame
+from curses_tools import draw_frame, read_controls
 from custom_sleep import async_sleep
 
 
 async def animate_spaceship(canvas, start_row: int, start_column: int, frames: list):
+    canvas.nodelay(True)  # make non block input
+    corrected_start_column = start_column - 2  # correcting depend on frame size
+    current_row, current_column = start_row, corrected_start_column
+
     while True:
+        diff_rows, diff_columns, press_enter = read_controls(canvas)
+        current_row += diff_rows
+        current_column += diff_columns
+
         for frame in frames:
-            draw_frame(canvas, start_row, start_column, frame)
+            draw_frame(canvas, current_row, current_column, frame)
             canvas.refresh()
 
             await async_sleep(2)
 
-            # стираем предыдущий кадр, прежде чем рисовать новый
-            draw_frame(canvas, start_row, start_column, frame, negative=True)
+            # delete previous frame before next one
+            draw_frame(canvas, current_row, current_column, frame, negative=True)
